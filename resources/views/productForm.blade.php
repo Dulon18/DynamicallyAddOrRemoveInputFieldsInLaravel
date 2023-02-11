1130d3c9249307879e61061b869c30c8c4ef5b00
Dulon18 <dulon587@gmail.com>
2023-02-11 21:13:12 +0600

    design update

--- resources/views/productForm.blade.php
+++ resources/views/productForm.blade.php
@@ -4,135 +4,147 @@
 
 <head>
 
-    <title>Add/remove multiple input fields dynamically with Jquery Laravel 5.8</title>
+          <title>Add/remove multiple input fields dynamically</title>
 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
+          <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+                    integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD"
+                    crossorigin="anonymous">
 
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+          <script src="//ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 
 </head>
 
 <body>
 
-   
 
-<div class="container">
 
-    <h2 align="center">Add/remove multiple input fields dynamically with Laravel</h2> 
+          <div class="container">
 
-   
+                    <h2 class=" text-center m-3 fw-bold text-success">Add / <span class="text-danger">Remove</span>
+                              Multiple
+                              input
+                              fields
+                              Dynamically
+                              with
+                              Laravel
+                    </h2>
 
-    <form action="{{ route('storemoreProduct') }}" method="POST">
+                    <form action="{{ route('storemoreProduct') }}" method="POST">
 
-        @csrf
+                              @csrf
+                              @if ($errors->any())
 
-   
+                              <div class="alert alert-danger">
 
-        @if ($errors->any())
+                                        <ul>
 
-            <div class="alert alert-danger">
+                                                  @foreach ($errors->all() as $error)
 
-                <ul>
+                                                  <li>{{ $error }}</li>
 
-                    @foreach ($errors->all() as $error)
+                                                  @endforeach
 
-                        <li>{{ $error }}</li>
+                                        </ul>
 
-                    @endforeach
+                              </div>
 
-                </ul>
+                              @endif
 
-            </div>
 
-        @endif
 
-   
+                              @if (Session::has('success'))
 
-        @if (Session::has('success'))
+                              <div class="alert alert-success text-center">
 
-            <div class="alert alert-success text-center">
+                                        <a href="#" class="close" data-dismiss="alert" aria-label="close">×</a>
 
-                <a href="#" class="close" data-dismiss="alert" aria-label="close">×</a>
+                                        <p>{{ Session::get('success') }}</p>
 
-                <p>{{ Session::get('success') }}</p>
+                              </div>
 
-            </div>
+                              @endif
 
-        @endif
 
-   
 
-        <table class="table table-bordered" id="dynamicTable">  
+                              <table class="table table-bordered" id="dynamicTable">
 
-            <tr>
+                                        <tr>
 
-                <th> Product Name</th>
+                                                  <th class="text-center"> Product Name</th>
 
-                <th>Qty</th>
+                                                  <th class="text-center">Product Qty</th>
 
-                <th>Price</th>
+                                                  <th class=" text-center">Price</th>
 
-                <th>Action</th>
+                                                  <th class=" text-center">Action</th>
 
-            </tr>
+                                        </tr>
 
-            <tr>  
+                                        <tr>
 
-                <td><input type="text" name="addProduct[0][name]" placeholder="Enter your Name" class="form-control" /></td>  
+                                                  <td><input type=" text" name="addProduct[0][name]"
+                                                                      placeholder="Enter your Name"
+                                                                      class="form-control" />
+                                                  </td>
 
-                <td><input type="text" name="addProduct[0][qty]" placeholder="Enter your Qty" class="form-control" /></td>  
+                                                  <td><input type="text" name="addProduct[0][qty]"
+                                                                      placeholder="Enter your Qty"
+                                                                      class="form-control" /></td>
 
-                <td><input type="text" name="addProduct[0][price]" placeholder="Enter your Price" class="form-control" /></td>  
+                                                  <td><input type="text" name="addProduct[0][price]"
+                                                                      placeholder="Enter your Price"
+                                                                      class="form-control" /></td>
 
-                <td><button type="button" name="add" id="add" class="btn btn-success">Add More</button></td>  
+                                                  <td class="text-center"><button type=" button" name="add" id="add"
+                                                                      class="btn btn-success">Add More</button></td>
 
-            </tr>  
+                                        </tr>
 
-        </table> 
+                              </table>
 
-    
 
-        <button type="submit" class="btn btn-success">Save</button>
 
-    </form>
+                              <button type="submit" class="btn btn-success">Save</button>
 
-</div>
+                    </form>
 
-   
+          </div>
 
-<script type="text/javascript">
 
-   
 
-    var i = 0;
+          <script type="text/javascript">
+          var i = 0;
 
-       
 
-    $("#add").click(function(){
 
-   
+          $("#add").click(function() {
 
-        ++i;
 
-   
 
-        $("#dynamicTable").append('<tr><td><input type="text" name="addProduct['+i+'][name]" placeholder="Enter your Name" class="form-control" /></td><td><input type="text" name="addProduct['+i+'][qty]" placeholder="Enter your Qty" class="form-control" /></td><td><input type="text" name="addProduct['+i+'][price]" placeholder="Enter your Price" class="form-control" /></td><td><button type="button" class="btn btn-danger remove-tr">Remove</button></td></tr>');
+                    ++i
 
-    });
 
-   
 
-    $(document).on('click', '.remove-tr', function(){  
+                    $("#dynamicTable").append('<tr><td><input type="text" name="addProduct[' + i +
+                              '][name]" placeholder="Enter your Name" class="form-control" /></td><td><input type="text" name="addProduct[' +
+                              i +
+                              '][qty]" placeholder="Enter your Qty" class="form-control" /></td><td><input type="text" name="addProduct[' +
+                              i +
+                              '][price]" placeholder="Enter your Price" class="form-control" /></td ><td class="text-center"><button type="button" class="btn btn-danger remove-tr">Remove</button></td></tr>'
+                    );
 
-         $(this).parents('tr').remove();
+          });
 
-    });  
 
-   
 
-</script>
+          $(document).on('click', '.remove-tr', function() {
 
-  
+                    $(this).parents('tr').remove();
+
+          });
+          </script>
+
+
 
 </body>
 
